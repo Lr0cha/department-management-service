c9d769b8c2b74d56496cc7799eb1afb30e2b74ed
Lucas Rocha <ldrocha16@gmail.com>
2024-12-13 08:36:16 -0300

    Inicialização projeto

---
+++ src/test/java/com/example/UserDept/UserDeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserDept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
